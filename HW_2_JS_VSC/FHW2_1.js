0571bddc7bf046130d75da61b350c12f5f252aac
DmitriyZakharenko <103507464+DmitriyZakharenko@users.noreply.github.com>
2022-04-20 05:48:30 +0700

    EndV

--- HW_2_JS_VSC/FHW2_1.js
+++ HW_2_JS_VSC/FHW2_1.js
@@ -1,9 +1,10 @@
-
 const checkAge = function(age) {
-   
-            let age_1 = 10
-            let age_2 = 18
-            let age_3 = 60
+    let age_2 = 18
+    let age_3 = 60
+    if (typeof(age) != "number"){
+        console.log ("Error")
+    }   
+    else {
             if (age < age_2){
             console.log ("You don’t have access cause your age is", age, "It’s less then")
             } 
@@ -17,8 +18,7 @@ const checkAge = function(age) {
             console.log("Technical work")
             }
         }
-    
+    }
 checkAge (17)
 checkAge (18)
 checkAge (61)
-// checkAge ('2')
